0b77057ee79992f1f8b420c1cf67dc6daae2fdbd
laizu akram <laizuakram98@gmail.com>
2023-02-28 11:59:42 +0600

    conditional booking after booked

--- index.js
+++ index.js
@@ -18,7 +18,27 @@ async function run(){
     try{
         await client.connect();
         const doctorsCollection = client.db('doctorsCollection').collection('doctors');
+        const bookingCollection = client.db('bookingCollection').collection('bookings');
       
+
+        //booking appointment
+        app.post('/bookings', async(req, res) =>{
+            const data = req.body;
+            const query = {
+                specialist: data.specialist
+            }
+            const booked = await bookingCollection.find(query).toArray();
+            if(booked.length){
+                const message=`You already booked on ${data.specialist} please try another one`
+                return res.send({acknowledged: false, message})
+            }
+            const result = await bookingCollection.insertOne(data);
+            
+
+            res.send(result);
+        })
+
+        // get all doctors
         app.get('/doctors', async (req, res) => {
             const query = {};
             const result =await doctorsCollection.find(query).toArray();
